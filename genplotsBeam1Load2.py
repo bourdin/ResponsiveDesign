77b77308d26f1e9e225b324596d9d44d74d9c57e
Blaise Bourdin <bourdin@mcmaster.ca>
2024-07-15 14:45:55 -0400

    Added codes

---
+++ genplotsBeam1Load2.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+import sys
+
+
+
+db = sys.argv[-1]
+prefix,ext = os.path.splitext(sys.argv[-1])
+
+OpenDatabase("localhost:{}".format(db), 0)
+SetTimeSliderState(TimeSliderGetNStates()-1)
+
+
+###
+### Annotations
+###
+
+# Logging for SetAnnotationObjectOptions is not implemented yet.
+AnnotationAtts = AnnotationAttributes()
+AnnotationAtts.axes2D.visible = 1
+AnnotationAtts.axes2D.autoSetTicks = 1
+AnnotationAtts.axes2D.autoSetScaling = 1
+AnnotationAtts.axes2D.lineWidth = 0
+AnnotationAtts.axes2D.tickLocation = AnnotationAtts.axes2D.Outside  # Inside, Outside, Both
+AnnotationAtts.axes2D.tickAxes = AnnotationAtts.axes2D.BottomLeft  # Off, Bottom, Left, BottomLeft, All
+AnnotationAtts.axes2D.xAxis.title.visible = 0
+AnnotationAtts.axes2D.xAxis.title.font.font = AnnotationAtts.axes2D.xAxis.title.font.Courier  # Arial, Courier, Times
+AnnotationAtts.axes2D.xAxis.title.font.scale = 1
+AnnotationAtts.axes2D.xAxis.title.font.useForegroundColor = 1
+AnnotationAtts.axes2D.xAxis.title.font.color = (0, 0, 0, 255)
+AnnotationAtts.axes2D.xAxis.title.font.bold = 1
+AnnotationAtts.axes2D.xAxis.title.font.italic = 1
+AnnotationAtts.axes2D.xAxis.title.userTitle = 0
+AnnotationAtts.axes2D.xAxis.title.userUnits = 0
+AnnotationAtts.axes2D.xAxis.title.title = "X-Axis"
+AnnotationAtts.axes2D.xAxis.title.units = ""
+AnnotationAtts.axes2D.xAxis.label.visible = 1
+AnnotationAtts.axes2D.xAxis.label.font.font = AnnotationAtts.axes2D.xAxis.label.font.Courier  # Arial, Courier, Times
+AnnotationAtts.axes2D.xAxis.label.font.scale = 1
+AnnotationAtts.axes2D.xAxis.label.font.useForegroundColor = 1
+AnnotationAtts.axes2D.xAxis.label.font.color = (0, 0, 0, 255)
+AnnotationAtts.axes2D.xAxis.label.font.bold = 1
+AnnotationAtts.axes2D.xAxis.label.font.italic = 1
+AnnotationAtts.axes2D.xAxis.label.scaling = 0
+AnnotationAtts.axes2D.xAxis.tickMarks.visible = 1
+AnnotationAtts.axes2D.xAxis.tickMarks.majorMinimum = 0
+AnnotationAtts.axes2D.xAxis.tickMarks.majorMaximum = 1
+AnnotationAtts.axes2D.xAxis.tickMarks.minorSpacing = 0.02
+AnnotationAtts.axes2D.xAxis.tickMarks.majorSpacing = 0.2
+AnnotationAtts.axes2D.xAxis.grid = 0
+AnnotationAtts.axes2D.yAxis.title.visible = 0
+AnnotationAtts.axes2D.yAxis.title.font.font = AnnotationAtts.axes2D.yAxis.title.font.Courier  # Arial, Courier, Times
+AnnotationAtts.axes2D.yAxis.title.font.scale = 1
+AnnotationAtts.axes2D.yAxis.title.font.useForegroundColor = 1
+AnnotationAtts.axes2D.yAxis.title.font.color = (0, 0, 0, 255)
+AnnotationAtts.axes2D.yAxis.title.font.bold = 1
+AnnotationAtts.axes2D.yAxis.title.font.italic = 1
+AnnotationAtts.axes2D.yAxis.title.userTitle = 0
+AnnotationAtts.axes2D.yAxis.title.userUnits = 0
+AnnotationAtts.axes2D.yAxis.title.title = "Y-Axis"
+AnnotationAtts.axes2D.yAxis.title.units = ""
+AnnotationAtts.axes2D.yAxis.label.visible = 1
+AnnotationAtts.axes2D.yAxis.label.font.font = AnnotationAtts.axes2D.yAxis.label.font.Courier  # Arial, Courier, Times
+AnnotationAtts.axes2D.yAxis.label.font.scale = 1
+AnnotationAtts.axes2D.yAxis.label.font.useForegroundColor = 1
+AnnotationAtts.axes2D.yAxis.label.font.color = (0, 0, 0, 255)
+AnnotationAtts.axes2D.yAxis.label.font.bold = 1
+AnnotationAtts.axes2D.yAxis.label.font.italic = 1
+AnnotationAtts.axes2D.yAxis.label.scaling = 0
+AnnotationAtts.axes2D.yAxis.tickMarks.visible = 1
+AnnotationAtts.axes2D.yAxis.tickMarks.majorMinimum = 0
+AnnotationAtts.axes2D.yAxis.tickMarks.majorMaximum = 1
+AnnotationAtts.axes2D.yAxis.tickMarks.minorSpacing = 0.02
+AnnotationAtts.axes2D.yAxis.tickMarks.majorSpacing = 0.2
+AnnotationAtts.axes2D.yAxis.grid = 0
+AnnotationAtts.userInfoFlag = 0
+AnnotationAtts.userInfoFont.font = AnnotationAtts.userInfoFont.Arial  # Arial, Courier, Times
+AnnotationAtts.userInfoFont.scale = 1
+AnnotationAtts.userInfoFont.useForegroundColor = 1
+AnnotationAtts.userInfoFont.color = (0, 0, 0, 255)
+AnnotationAtts.userInfoFont.bold = 0
+AnnotationAtts.userInfoFont.italic = 0
+AnnotationAtts.databaseInfoFlag = 0
+AnnotationAtts.timeInfoFlag = 1
+AnnotationAtts.databaseInfoFont.font = AnnotationAtts.databaseInfoFont.Arial  # Arial, Courier, Times
+AnnotationAtts.databaseInfoFont.scale = 1
+AnnotationAtts.databaseInfoFont.useForegroundColor = 1
+AnnotationAtts.databaseInfoFont.color = (0, 0, 0, 255)
+AnnotationAtts.databaseInfoFont.bold = 0
+AnnotationAtts.databaseInfoFont.italic = 0
+AnnotationAtts.databaseInfoExpansionMode = AnnotationAtts.File  # File, Directory, Full, Smart, SmartDirectory
+AnnotationAtts.databaseInfoTimeScale = 1
+AnnotationAtts.databaseInfoTimeOffset = 0
+AnnotationAtts.legendInfoFlag = 0
+AnnotationAtts.backgroundColor = (255, 255, 255, 255)
+AnnotationAtts.foregroundColor = (0, 0, 0, 255)
+AnnotationAtts.gradientBackgroundStyle = AnnotationAtts.Radial  # TopToBottom, BottomToTop, LeftToRight, RightToLeft, Radial
+AnnotationAtts.gradientColor1 = (0, 0, 255, 255)
+AnnotationAtts.gradientColor2 = (0, 0, 0, 255)
+AnnotationAtts.backgroundMode = AnnotationAtts.Solid  # Solid, Gradient, Image, ImageSphere
+AnnotationAtts.backgroundImage = ""
+AnnotationAtts.imageRepeatX = 1
+AnnotationAtts.imageRepeatY = 1
+AnnotationAtts.axesArray.visible = 1
+AnnotationAtts.axesArray.ticksVisible = 1
+AnnotationAtts.axesArray.autoSetTicks = 1
+AnnotationAtts.axesArray.autoSetScaling = 1
+AnnotationAtts.axesArray.lineWidth = 0
+AnnotationAtts.axesArray.axes.title.visible = 1
+AnnotationAtts.axesArray.axes.title.font.font = AnnotationAtts.axesArray.axes.title.font.Arial  # Arial, Courier, Times
+AnnotationAtts.axesArray.axes.title.font.scale = 1
+AnnotationAtts.axesArray.axes.title.font.useForegroundColor = 1
+AnnotationAtts.axesArray.axes.title.font.color = (0, 0, 0, 255)
+AnnotationAtts.axesArray.axes.title.font.bold = 0
+AnnotationAtts.axesArray.axes.title.font.italic = 0
+AnnotationAtts.axesArray.axes.title.userTitle = 0
+AnnotationAtts.axesArray.axes.title.userUnits = 0
+AnnotationAtts.axesArray.axes.title.title = ""
+AnnotationAtts.axesArray.axes.title.units = ""
+AnnotationAtts.axesArray.axes.label.visible = 1
+AnnotationAtts.axesArray.axes.label.font.font = AnnotationAtts.axesArray.axes.label.font.Arial  # Arial, Courier, Times
+AnnotationAtts.axesArray.axes.label.font.scale = 1
+AnnotationAtts.axesArray.axes.label.font.useForegroundColor = 1
+AnnotationAtts.axesArray.axes.label.font.color = (0, 0, 0, 255)
+AnnotationAtts.axesArray.axes.label.font.bold = 0
+AnnotationAtts.axesArray.axes.label.font.italic = 0
+AnnotationAtts.axesArray.axes.label.scaling = 0
+AnnotationAtts.axesArray.axes.tickMarks.visible = 1
+AnnotationAtts.axesArray.axes.tickMarks.majorMinimum = 0
+AnnotationAtts.axesArray.axes.tickMarks.majorMaximum = 1
+AnnotationAtts.axesArray.axes.tickMarks.minorSpacing = 0.02
+AnnotationAtts.axesArray.axes.tickMarks.majorSpacing = 0.2
+AnnotationAtts.axesArray.axes.grid = 0
+SetAnnotationAttributes(AnnotationAtts)
+
+###
+### export options
+###
+SaveWindowAtts = SaveWindowAttributes()
+SaveWindowAtts.outputToCurrentDirectory = 1
+SaveWindowAtts.outputDirectory = "."
+SaveWindowAtts.fileName = "{}-design".format(prefix)
+SaveWindowAtts.family = 0
+SaveWindowAtts.format = SaveWindowAtts.PNG  # BMP, CURVE, JPEG, OBJ, PNG, POSTSCRIPT, POVRAY, PPM, RGB, STL, TIFF, ULTRA, VTK, PLY, EXR
+SaveWindowAtts.width = 4096
+SaveWindowAtts.height = 4096
+SaveWindowAtts.screenCapture = 0
+SaveWindowAtts.saveTiled = 0
+SaveWindowAtts.quality = 80
+SaveWindowAtts.progressive = 0
+SaveWindowAtts.binary = 0
+SaveWindowAtts.stereo = 0
+SaveWindowAtts.compression = SaveWindowAtts.NONE  # NONE, PackBits, Jpeg, Deflate, LZW
+SaveWindowAtts.forceMerge = 1
+SaveWindowAtts.resConstraint = SaveWindowAtts.NoConstraint  # NoConstraint, EqualWidthHeight, ScreenProportions
+SaveWindowAtts.pixelData = 1
+SaveWindowAtts.opts.types = ()
+SaveWindowAtts.opts.help = ""
+SetSaveWindowAttributes(SaveWindowAtts)
+
+###
+### Structural material
+###
+AddPlot("Pseudocolor", "Structural", 1, 1)
+
+PseudocolorAtts = PseudocolorAttributes()
+PseudocolorAtts.scaling = PseudocolorAtts.Linear  # Linear, Log, Skew
+PseudocolorAtts.skewFactor = 1
+PseudocolorAtts.limitsMode = PseudocolorAtts.OriginalData  # OriginalData, ActualData
+PseudocolorAtts.minFlag = 0
+PseudocolorAtts.min = 0
+PseudocolorAtts.useBelowMinColor = 0
+PseudocolorAtts.belowMinColor = (0, 0, 0, 255)
+PseudocolorAtts.maxFlag = 0
+PseudocolorAtts.max = 1
+PseudocolorAtts.useAboveMaxColor = 0
+PseudocolorAtts.aboveMaxColor = (0, 0, 0, 255)
+PseudocolorAtts.centering = PseudocolorAtts.Natural  # Natural, Nodal, Zonal
+PseudocolorAtts.colorTableName = "gray"
+PseudocolorAtts.invertColorTable = 1
+PseudocolorAtts.opacityType = PseudocolorAtts.FullyOpaque  # ColorTable, FullyOpaque, Constant, Ramp, VariableRange
+PseudocolorAtts.opacityVariable = ""
+PseudocolorAtts.opacity = 1
+PseudocolorAtts.opacityVarMin = 0
+PseudocolorAtts.opacityVarMax = 1
+PseudocolorAtts.opacityVarMinFlag = 0
+PseudocolorAtts.opacityVarMaxFlag = 0
+PseudocolorAtts.pointSize = 0.05
+PseudocolorAtts.pointType = PseudocolorAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+PseudocolorAtts.pointSizeVarEnabled = 0
+PseudocolorAtts.pointSizeVar = "default"
+PseudocolorAtts.pointSizePixels = 2
+PseudocolorAtts.lineType = PseudocolorAtts.Line  # Line, Tube, Ribbon
+PseudocolorAtts.lineWidth = 0
+PseudocolorAtts.tubeResolution = 10
+PseudocolorAtts.tubeRadiusSizeType = PseudocolorAtts.FractionOfBBox  # Absolute, FractionOfBBox
+PseudocolorAtts.tubeRadiusAbsolute = 0.125
+PseudocolorAtts.tubeRadiusBBox = 0.005
+PseudocolorAtts.tubeRadiusVarEnabled = 0
+PseudocolorAtts.tubeRadiusVar = ""
+PseudocolorAtts.tubeRadiusVarRatio = 10
+PseudocolorAtts.tailStyle = PseudocolorAtts.NONE  # NONE, Spheres, Cones
+PseudocolorAtts.headStyle = PseudocolorAtts.NONE  # NONE, Spheres, Cones
+PseudocolorAtts.endPointRadiusSizeType = PseudocolorAtts.FractionOfBBox  # Absolute, FractionOfBBox
+PseudocolorAtts.endPointRadiusAbsolute = 0.125
+PseudocolorAtts.endPointRadiusBBox = 0.05
+PseudocolorAtts.endPointResolution = 10
+PseudocolorAtts.endPointRatio = 5
+PseudocolorAtts.endPointRadiusVarEnabled = 0
+PseudocolorAtts.endPointRadiusVar = ""
+PseudocolorAtts.endPointRadiusVarRatio = 10
+PseudocolorAtts.renderSurfaces = 1
+PseudocolorAtts.renderWireframe = 0
+PseudocolorAtts.renderPoints = 0
+PseudocolorAtts.smoothingLevel = 0
+PseudocolorAtts.legendFlag = 0
+PseudocolorAtts.lightingFlag = 0
+PseudocolorAtts.wireframeColor = (0, 0, 0, 0)
+PseudocolorAtts.pointColor = (0, 0, 0, 0)
+SetPlotOptions(PseudocolorAtts)
+
+DrawPlots()
+
+# SaveWindowAtts = SaveWindowAttributes()
+SaveWindowAtts.fileName = "{}-Struct".format(prefix)
+SetSaveWindowAttributes(SaveWindowAtts)
+SaveWindow()
+
+
+###
+### Responsive Material
+###
+ChangeActivePlotsVar("Responsive")
+
+# PseudocolorAtts.colorTableName = "Greens"
+# PseudocolorAtts.invertColorTable = 0
+# SetPlotOptions(PseudocolorAtts)
+DrawPlots()
+
+
+# SaveWindowAtts = SaveWindowAttributes()
+SaveWindowAtts.fileName = "{}-Resp".format(prefix)
+SetSaveWindowAttributes(SaveWindowAtts)
+SaveWindow()
+
+DeleteAllPlots()
+CloseDatabase(db)
+
+sys.exit(0)
